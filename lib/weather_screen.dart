97fc92cc4540993e02dac11677ff67f1ca822840
Divin_Dass <projectwithdivin@gmail.com>
2024-01-31 10:52:28 +0000

    UI Updated

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -10,15 +11,76 @@ class WeatherScreen extends StatelessWidget {
         title: const Text(
           'Weather App',
           style: TextStyle(
+            fontSize: 28,
             fontWeight: FontWeight.bold,
           ),
         ),
         centerTitle: true,
         actions: [
           GestureDetector(
-            child: const Icon(Icons.refresh),
+            child: const Icon(Icons.refresh, size: 32),
           )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            //main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 20,
+                      sigmaY: 20,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '32°C',
+                            style: TextStyle(
+                              fontSize: 38,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 64,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            //secondary card
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(height: 25),
+            //third card
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+          ],
+        ),
       ),
     );
   }
